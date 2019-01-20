6d0ce5ba3ce5905e833d6f1cf9ec8aa8b89de79a
Daichi Seki <10bvb25hal45016@gmail.com>
2019-01-20 22:54:15 +0900

    correct test code

--- tests/Feature/LoginApiTest.php
+++ tests/Feature/LoginApiTest.php
@@ -24,7 +24,7 @@ class LoginApiTest extends TestCase
     public function should_登録済みのユーザーを認証して返却する() {
         $response = $this->json('POST', route('login'), [
             'email'    => $this->user->email,
-            'password' => 'select',
+            'password' => 'secret',
         ]);
 
         $response
